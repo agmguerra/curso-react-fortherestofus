c3c82db44cb70b901b4b672b56e3cff263852551
AlexandreGuerra <agmguerra@gmail.com>
2020-05-08 00:57:52 -0300

    aplicação com context e reducer

--- complex-react-app/app/Main.js
+++ complex-react-app/app/Main.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState, useReducer, useEffect } from "react";
 import ReactDOM from "react-dom";
+import { useImmerReducer } from 'use-immer';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Axios from 'axios';
+Axios.defaults.baseURL = 'http://localhost:8080';
+
+import StateContext from './StateContext';
+import DispatchContext from './DispatchContext';
 
 // May components
 import Header from "./components/Header";
@@ -8,24 +15,79 @@ import HomeGuest from "./components/HomeGuest";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Terms from "./components/Terms";
+import Home from './components/Home'
+import CreatePost from './components/CreatePost';
+import ViewSinglePost from './components/ViewSinglePost';
+import FlashMessages from './components/FlashMessages';
 
 const Main = () => {
+
+  const initialState = {
+    loggedIn: Boolean(localStorage.getItem("complexappToken")),
+    flashMessages: [],
+    user: {
+      token: localStorage.getItem('complexappToken'),
+      username: localStorage.getItem('complexappUsername'),
+      avatar: localStorage.getItem('complexappAvatar')
+    }
+  };
+
+  const ourReducer = (draft, action) => {
+    switch (action.type) {
+      case 'login':
+        draft.loggedIn = true
+        draft.user = action.data
+        return
+      case 'logout':
+        draft.loggedIn = false
+        return
+      case 'flashMessage':
+        draft.flashMessages.push(action.value)
+        return
+    }
+  }
+
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState);
+
+  useEffect(() => {
+    if (state.loggedIn) {
+      localStorage.setItem('complexappToken', state.user.token);
+      localStorage.setItem('complexappUsername', state.user.username);
+      localStorage.setItem('complexappAvatar', state.user.avatar);
+    } else {
+      localStorage.removeItem('complexappToken');
+      localStorage.removeItem('complexappUsername');
+      localStorage.removeItem('complexappAvatar');
+    }
+  }, [state.loggedIn]);
+
   return (
-    <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route path="/" exact>
-          <HomeGuest />
-        </Route>
-        <Route path="/about-us">
-          <About />
-        </Route>
-        <Route path="/terms">
-          <Terms />
-        </Route>
-      </Switch>
-      <Footer />
-    </BrowserRouter>
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        <BrowserRouter>
+          <FlashMessages messages={state.flashMessages} />
+          <Header />
+          <Switch>
+            <Route path="/" exact>
+              {state.loggedIn ? <Home /> : <HomeGuest />}
+            </Route>
+            <Route path="/post/:id">
+              <ViewSinglePost />
+            </Route>
+            <Route path="/create-post">
+              <CreatePost />
+            </Route>
+            <Route path="/about-us">
+              <About />
+            </Route>
+            <Route path="/terms">
+              <Terms />
+            </Route>
+          </Switch>
+          <Footer />
+        </BrowserRouter>
+      </DispatchContext.Provider>
+    </StateContext.Provider>
   );
 };
 
