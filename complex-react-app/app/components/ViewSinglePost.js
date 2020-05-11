2645b6c6cdfa01643906d977d2f16c939fe05904
AlexandreGuerra <agmguerra@gmail.com>
2020-05-11 02:05:14 -0300

    ajuste

--- complex-react-app/app/components/ViewSinglePost.js
+++ complex-react-app/app/components/ViewSinglePost.js
@@ -1,14 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import { useParams, Link } from 'react-router-dom';
-import Axios from 'axios';
-import Page from './Page';
-import LoadingDotsIcon from './LoadingDotsIcon';
-import ReactMarkdown from 'react-markdown';
-import ReactTooltip from 'react-tooltip';
-import NotFound from './NotFound';
+import React, { useEffect, useState, useContext } from "react";
+import { useParams, Link, withRouter } from "react-router-dom";
+import Axios from "axios";
+import Page from "./Page";
+import LoadingDotsIcon from "./LoadingDotsIcon";
+import ReactMarkdown from "react-markdown";
+import ReactTooltip from "react-tooltip";
+import NotFound from "./NotFound";
+import StateContext from '../StateContext';
+import DispatchContext from '../DispatchContext';
 
-const ViewSinglePost = () => {
+const ViewSinglePost = (props) => {
 
+  const appState = useContext(StateContext)
+  const appDispatch = useContext(DispatchContext)
   const { id } = useParams();
   const [isLoading, setIsLoading] = useState(true);
   const [post, setPost] = useState();
@@ -18,10 +22,7 @@ const ViewSinglePost = () => {
     const ourRequest = Axios.CancelToken.source();
     const fetchPost = async () => {
       try {
-        const res = await Axios.get(
-          `/post/${id}`, 
-          { cancelToken: ourRequest.token }
-        );
+        const res = await Axios.get(`/post/${id}`, { cancelToken: ourRequest.token });
         setPost(res.data);
         setIsLoading(false);
       } catch (e) {
@@ -33,65 +34,91 @@ const ViewSinglePost = () => {
       //cancela o chamada do axios quando o usuário
       //clicou em algum link antes do resultado chegar
       ourRequest.cancel();
-    }
+    };
   }, []);
 
   if (!isLoading && !post) {
-    return <NotFound />
+    return <NotFound />;
   }
-  if (isLoading) 
+  if (isLoading)
     return (
-      <Page title="..."><LoadingDotsIcon /></Page>
-    )
+      <Page title="...">
+        <LoadingDotsIcon />
+      </Page>
+    );
 
   const date = new Date(post.createdDate);
   const dataFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+
+  const isOwner = () => {
+    if (appState.loggedIn) {
+      return appState.user.username == post.author.username
+    };
+    return false;
+
+  };
+
+  const deleteHandler = async () => {
+    const areYouSure = window.confirm('Do you really want do delete this post');
+    if (areYouSure) {
+      try {
+        const res = await Axios.delete(`/post/${id}`, {
+          data: {token: appState.user.token}
+        });
+        if (res.data == 'Success') {
+          appDispatch({ 
+            type: 'flashMessage', 
+            value: 'Post was succesfuly deleted.'
+          });
+
+          //2. redirect back to current user's profile
+          props.history.push(`/profile/${appState.user.username}`);
+        }
+      } catch (e) {
+        console.log('There was a problem');
+      }
+    }
+  }
+
   return (
     <Page title={post.title}>
       <div className="d-flex justify-content-between">
         <h2>{post.title}</h2>
-        <span className="pt-2">
-          <Link 
-            data-tip="Edit"
-            data-for="edit"
-            to={`/post/${post._id}/edit`}
-            className="text-primary mr-2" 
-          >
-            <i className="fas fa-edit"></i>
-          </Link>
-          <ReactTooltip id="edit" className="custom-tooltip"/>
-          {" "}
-          <a 
-            data-tip="Delete"
-            data-for="delete"
-            className="delete-post-button text-danger" 
-          >
-            <i className="fas fa-trash"></i>
-          </a>
-          <ReactTooltip id="delete" className="custom-tooltip"/>
-        </span>
+        {isOwner() && (
+          <span className="pt-2">
+            <Link 
+              data-tip="Edit" 
+              data-for="edit" 
+              to={`/post/${post._id}/edit`} 
+              className="text-primary mr-2"
+            >
+              <i className="fas fa-edit"></i>
+            </Link>
+            <ReactTooltip id="edit" className="custom-tooltip" />{" "}
+            <a 
+              onClick={deleteHandler}
+              data-tip="Delete" 
+              data-for="delete" 
+              className="delete-post-button text-danger"
+            >
+              <i className="fas fa-trash"></i>
+            </a>
+            <ReactTooltip id="delete" className="custom-tooltip" />
+          </span>
+        )}
       </div>
       <p className="text-muted small mb-4">
-        <Link 
-          to={`/profile/${post.author.username}`}>
-          <img 
-            className="avatar-tiny" 
-            src={post.author.avatar}
-          />
+        <Link to={`/profile/${post.author.username}`}>
+          <img className="avatar-tiny" src={post.author.avatar} />
         </Link>
         Posted by <Link to={`/profile/${post.author.username}`}>{post.author.username}</Link> on {dataFormatted}
       </p>
 
       <div className="body-content">
-        <ReactMarkdown 
-          source={post.body} 
-          allowedTypes={
-            ["paragraph", "strong", "emphasis", "text", "heading", "list", "listItem"]
-          } 
-        />
+        <ReactMarkdown source={post.body} allowedTypes={["paragraph", "strong", "emphasis", "text", "heading", "list", "listItem"]} />
       </div>
     </Page>
-  )
-}
+  );
+};
 
-export default ViewSinglePost
+export default withRouter(ViewSinglePost);
